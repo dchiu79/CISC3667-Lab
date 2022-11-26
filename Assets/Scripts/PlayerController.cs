dc1252d2f227b0bcad9eb991f8daa9c6a8453bf3
DC <78552552+DC@users.noreply.github.com>
2022-11-26 17:44:57 -0500

    Added image and killzone

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -20,5 +21,7 @@ public class PlayerController : MonoBehaviour
             position.x += 2.0f;
             Instantiate(pinFire, position, Quaternion.identity);
         }
+        if(gameObject.transform.position.y < -6.0f)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
